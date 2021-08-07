a46643728dd44592fd5ebf2af170288bf4263bd0
RajPatel <rvpatel7719@gmail.com>
2021-08-07 23:38:09 +0530

    Added Entity And Database Project, Added Business Entity And, Created Database

---
+++ Smart_Store.Database/SmartStoreContext.cs
@@ -0,0 +1,21 @@
+﻿using Smart_Store.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart_Store.Database
+{
+    public class SmartStoreContext : DbContext
+    {
+        public SmartStoreContext() : base("SmartStoreConnection")
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
