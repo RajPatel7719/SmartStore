0e0761620eb466506153b3bbeaa11df522d75b1a
RajPatel <rvpatel7719@gmail.com>
2021-08-12 18:43:18 +0530

    Add  Categories, Content, Images

--- Smart_Store.Web/Controllers/HomeController.cs
+++ Smart_Store.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Smart_Store.Services;
+using Smart_Store.Web.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,15 @@ namespace Smart_Store.Web.Controllers
 {
     public class HomeController : Controller
     {
+        CategoriesService categoriesService = new CategoriesService();
+
         public ActionResult Index()
         {
-            return View();
+            HomeViewModels model = new HomeViewModels();
+
+            model.Categories = categoriesService.GetCategories();
+
+            return View(model);
         }
 
         public ActionResult About()
---
+++ Smart_Store.Web/ViewModels/HomeViewModels.cs
@@ -0,0 +1,14 @@
+﻿using Smart_Store.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Smart_Store.Web.ViewModels
+{
+    public class HomeViewModels
+    {
+        public List<Category> Categories { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
